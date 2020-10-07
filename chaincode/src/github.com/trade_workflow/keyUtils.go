eb0754b75fad0403e450d1648f7557bf93ee5c21
Miguel Torrijos <miguel.torrijos@vipera.com>
2020-10-07 11:20:33 +0200

    Updated SDK links

--- chaincode/src/github.com/trade_workflow/keyUtils.go
+++ chaincode/src/github.com/trade_workflow/keyUtils.go
@@ -17,7 +17,7 @@
 package main
 
 import (
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 )
 
 func getTradeKey(stub shim.ChaincodeStubInterface, tradeID string) (string, error) {
